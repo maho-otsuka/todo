3e1e48ea3127b407a95d3485f43e5eaa17470d10
maho-otsuka <71184037+maho-otsuka@users.noreply.github.com>
2022-01-03 23:19:26 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -177,4 +177,4 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
     );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />　<h1>Hello world!</h1>, document.getElementById('root'));
